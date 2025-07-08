d22cb8aaf3e399b628c55aaf692998eee3ca2527
shubham khatik <40956270+shubhamkhatik@users.noreply.github.com>
2025-07-08 15:03:06 +0530

    errror handle

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -23,20 +23,33 @@ export async function createInvoice(formData: FormData) {
     amount: formData.get("amount"),
     status: formData.get("status"),
   });
-  // store monetary values in cents in your database to eliminate JavaScript floating-point errors and ensure greater accuracy.
-  const amountInCents = amount * 100;
-  const date = new Date().toISOString().split("T")[0];
-  await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `;
-  // Since you're updating the data displayed in the invoices route, you want to clear this cache and trigger a new request to the server
-  revalidatePath("/dashboard/invoices");
-  redirect("/dashboard/invoices");
   // Test it out: console is from terminal,not browser because its server action
   console.log("typeof amount:", typeof amount, amount);
   console.log("typeof customerId:", typeof customerId, customerId);
   console.log("typeof status:", typeof status, status);
+  // store monetary values in cents in your database to eliminate JavaScript floating-point errors and ensure greater accuracy.
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split("T")[0];
+  try {
+    await sql`
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+  } catch (error) {
+    console.error(error);
+  }
+  // Since you're updating the data displayed in the invoices route, you want to clear this cache and trigger a new request to the server
+  // Note how redirect is being called outside of the try/catch block. This is because redirect works by throwing an error
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
+
+  // try {
+  //   revalidatePath("/dashboard/invoices");
+  //   redirect("/dashboard/invoices");
+  //   console.log("This won't execute");
+  // } catch (error) {
+  //   console.log("Caught error:", error); // You'll see the NEXT_REDIRECT error
+  // }
 }
 
 // Use Zod to update the expected types
@@ -48,20 +61,23 @@ export async function updateInvoice(id: string, formData: FormData) {
     amount: formData.get("amount"),
     status: formData.get("status"),
   });
-
   const amountInCents = amount * 100;
-
-  await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
-  `;
-
+  try {
+    await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+      `;
+  } catch (error) {
+    // We'll log the error to the console for now
+    console.error(error);
+  }
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
 }
 
 export async function deleteInvoice(id: string) {
+  // throw new Error('Failed to Delete Invoice'); // error.tsx will catch this error
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath("/dashboard/invoices");
 }
