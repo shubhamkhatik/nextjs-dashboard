6f23e7a470a092f0f2238e2daf52bb8ab4674431
shubham khatik <40956270+shubhamkhatik@users.noreply.github.com>
2025-07-09 12:52:13 +0530

    eslint-plugin-jsx-a11y, server-side form validation, React useActionState hook

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -9,24 +9,43 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(), //coerce (change) from a string to a number while also validating its type.
-  status: z.enum(["pending", "paid"]),
+  customerId: z.string({
+    invalid_type_error: "Please select a customer.",
+  }),
+  //coerce (change) from a string to a number while also validating its type.
+  amount: z.coerce
+    .number()
+    .gt(0, { message: "Please enter an amount greater than $0." }),
+  status: z.enum(["pending", "paid"], {
+    invalid_type_error: "Please select an invoice status.",
+  }),
   date: z.string(),
 });
 
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
-  const { customerId, amount, status } = CreateInvoice.parse({
+export async function createInvoice(prevState: State, formData: FormData) {
+  const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get("customerId"),
     amount: formData.get("amount"),
     status: formData.get("status"),
   });
-  // Test it out: console is from terminal,not browser because its server action
-  console.log("typeof amount:", typeof amount, amount);
-  console.log("typeof customerId:", typeof customerId, customerId);
-  console.log("typeof status:", typeof status, status);
+  // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing Fields. Failed to Create Invoice.",
+    };
+  }
+  const { customerId, amount, status } = validatedFields.data;
   // store monetary values in cents in your database to eliminate JavaScript floating-point errors and ensure greater accuracy.
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split("T")[0];
@@ -36,7 +55,9 @@ export async function createInvoice(formData: FormData) {
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
   } catch (error) {
-    console.error(error);
+    return {
+      message: "Database Error: Failed to Create Invoice.",
+    };
   }
   // Since you're updating the data displayed in the invoices route, you want to clear this cache and trigger a new request to the server
   // Note how redirect is being called outside of the try/catch block. This is because redirect works by throwing an error
@@ -55,12 +76,23 @@ export async function createInvoice(formData: FormData) {
 // Use Zod to update the expected types
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(
+  id: string,
+  prevState: State,
+  formData: FormData
+) {
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get("customerId"),
     amount: formData.get("amount"),
     status: formData.get("status"),
   });
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing Fields. Failed to Update Invoice.",
+    };
+  }
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
   try {
     await sql`
@@ -69,8 +101,7 @@ export async function updateInvoice(id: string, formData: FormData) {
         WHERE id = ${id}
       `;
   } catch (error) {
-    // We'll log the error to the console for now
-    console.error(error);
+    return { message: "Database Error: Failed to Update Invoice." };
   }
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
